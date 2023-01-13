ebb8b79cf3017025823bd9da651740db19d47c6e
pedrofalco <fpfalco@gmail.com>
2023-01-13 17:07:09 -0300

    actualizacion de readme y numero de mails

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 ## Basurero 🗑
 
-Un ejercicio para [UR](https://www.instagram.com/urniversidad/) que consiste en una aplicacion web que busca mails entre mi carpeta de spam de Gmail que es usado como prompt para generar una imagen con [DALL-E](https://openai.com/dall-e-2/).
+`ES`
+
+Un ejercicio para [UR](https://www.instagram.com/urniversidad/) que consiste en una aplicacion web que se conecta a la API de Google para buscar entre la carpeta de spam de mi mail que es usado como prompt para generar una imagen con la API de [DALL-E](https://openai.com/dall-e-2/).
+
+--> <https://entre-la-basura.glitch.me/>
+
+---
+
+`EN`
+An exercise for [UR](https://www.instagram.com/urniversidad/) consisting of a web application that connects to the Google API to search through the spam folder of my mail which is used as a prompt to generate an image with the [DALL-E](https://openai.com/dall-e-2/) API.
 
 --> <https://entre-la-basura.glitch.me/>
 
 👋 Si lo usas, si tenés algún tipo de feedback o problema, hacemelo llegar por favor :)
+
+🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂🪂
--- index.js
+++ index.js
@@ -1,149 +1,144 @@
-console.log('app running...');
+console.log("app running...");
 
-const dotenv = require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-const fs = require('fs');
-const {
-    Configuration,
-    OpenAIApi
-} = require("openai");
+const dotenv = require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+const { Configuration, OpenAIApi } = require("openai");
 const fsPromises = fs.promises;
-const {
-    google
-} = require('googleapis');
+const { google } = require("googleapis");
 
 const port = process.env.PORT || 1111;
 const app = express();
-const server = app.listen(port, () => console.log(`listening @ ${port} --> http://localhost:${port}`));
-app.use(express.static('public'));
-app.use(express.json({
-    limit: '2mb'
-}));
+const server = app.listen(port, () =>
+	console.log(`listening @ ${port} --> http://localhost:${port}`)
+);
+app.use(express.static("public"));
+app.use(
+	express.json({
+		limit: "2mb",
+	})
+);
 app.use(cors());
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+	apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-const max = 26;
+const max = 39;
 
-app.get('/api', async(request, response) => {
+app.get("/api", async (request, response) => {
+	console.log("resquest...");
 
-    console.log('resquest...');
+	let subs = await new ReadEmail("_dominio").getSpam();
 
-    let subs = await new ReadEmail('_dominio').getSpam();
+	// console.log(subs[getRandomInt(0, 26)])
 
-    // console.log(subs[getRandomInt(0, 26)])
+	const prompt = subs[getRandomInt(0, max)];
 
-    const prompt = subs[getRandomInt(0, max)];
+	try {
+		const data = await generateImageRequest(prompt);
+		response.json({
+			status: "success",
+			image: data,
+			prompt: prompt,
+		});
+		console.log("prompt:", prompt);
+	} catch (err) {
+		console.log("llegaste al limite de búsquedas, esperá un momento...");
+		console.log(err);
+		response.json({
+			status: "error",
+			image: null,
+			prompt: prompt,
+		});
+	}
 
-    try {
-        const data = await generateImageRequest(prompt);
-        response.json({
-            'status': 'success',
-            'image': data,
-            'prompt': prompt
-        });
-        console.log('prompt:', prompt);
-
-    } catch (err) {
-        console.log('llegaste al limite de búsquedas, esperá un momento...');
-        console.log(err)
-        response.json({
-            'status': 'error',
-            'image': null,
-            'prompt': prompt
-        });
-    };
-
-    console.log('response sent');
+	console.log("response sent");
 });
 
-let generateImageRequest = async prompt => {
+let generateImageRequest = async (prompt) => {
+	console.log("generating image...");
 
-    console.log('generating image...');
+	const response = await openai.createImage({
+		prompt: prompt,
+		n: 1,
+		size: "512x512",
+		response_format: "b64_json",
+	});
 
-    const response = await openai.createImage({
-        prompt: prompt,
-        n: 1,
-        size: "512x512",
-        response_format: 'b64_json'
-    });
-
-    // downloadFile(response.data.data[0].url);
-    // console.log(response.data.data[0].b64_json)
-    let image_url = response.data.data;
-    return image_url;
+	// downloadFile(response.data.data[0].url);
+	// console.log(response.data.data[0].b64_json)
+	let image_url = response.data.data;
+	return image_url;
 };
 
 class ReadEmail {
-    constructor() {
-        this.auth = null;
-        this.message = null;
-    };
+	constructor() {
+		this.auth = null;
+		this.message = null;
+	}
 
-    async getSpam() {
-        this.auth = await this.getAuthorize();
-        this.message = await this.getLastMessage(max);
-        return this.message
-    };
+	async getSpam() {
+		this.auth = await this.getAuthorize();
+		this.message = await this.getLastMessage(max);
+		return this.message;
+	}
 
-    async getAuthorize() {
-        const oAuth2Client = await new google.auth.OAuth2(
-            process.env.CLIENT_ID,
-            process.env.CLIENT_SECRET,
-            process.env.REDIRECT_URIS,
-        );
+	async getAuthorize() {
+		const oAuth2Client = await new google.auth.OAuth2(
+			process.env.CLIENT_ID,
+			process.env.CLIENT_SECRET,
+			process.env.REDIRECT_URIS
+		);
 
-        await oAuth2Client.setCredentials({
-            type: process.env.TYPE,
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET,
-            refresh_token: process.env.REFRESH_TOKEN
-        });
+		await oAuth2Client.setCredentials({
+			type: process.env.TYPE,
+			client_id: process.env.CLIENT_ID,
+			client_secret: process.env.CLIENT_SECRET,
+			refresh_token: process.env.REFRESH_TOKEN,
+		});
 
-        return oAuth2Client;
-    };
+		return oAuth2Client;
+	}
 
-    async getLastMessage(max) {
-        let subjects = []
-        const gmail = await google.gmail({
-            version: 'v1',
-            auth: this.auth
-        });
-        var request = await gmail.users.messages.list({
-            userId: 'me',
-            labelIds: 'SPAM',
-            maxResults: max,
-        });
+	async getLastMessage(max) {
+		let subjects = [];
+		const gmail = await google.gmail({
+			version: "v1",
+			auth: this.auth,
+		});
+		var request = await gmail.users.messages.list({
+			userId: "me",
+			labelIds: "SPAM",
+			maxResults: max,
+		});
 
-        for (let i = 0; i <= max - 1; i++) {
-
-            let id = request.data.messages[i].id;
-            var request2 = await gmail.users.messages.get({
-                userId: 'me',
-                id: id,
-            });
-            let msg = request2.data.snippet;
-            subjects.push(msg);
-        };
-        return subjects;
-    };
-};
-
-
-const getRandomInt = (min, max) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
+		for (let i = 0; i <= max - 1; i++) {
+			let id = request.data.messages[i].id;
+			var request2 = await gmail.users.messages.get({
+				userId: "me",
+				id: id,
+			});
+			let msg = request2.data.snippet;
+			subjects.push(msg);
+		}
+		return subjects;
+	}
 }
 
-const downloadFile = async(url) => {
-    const response = await fetch(url);
-    const buffer = await response.buffer();
-    console.log(buffer)
-    fs.writeFile(`./image.jpg`, buffer, () =>
-        console.log('finished downloading!'));
+const getRandomInt = (min, max) => {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min);
+};
+
+const downloadFile = async (url) => {
+	const response = await fetch(url);
+	const buffer = await response.buffer();
+	console.log(buffer);
+	fs.writeFile(`./image.jpg`, buffer, () =>
+		console.log("finished downloading!")
+	);
 };
